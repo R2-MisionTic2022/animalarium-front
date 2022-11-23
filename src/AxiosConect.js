cce5fd4602b025a4de02c630dd2c16d45db074eb
Natalhy Corredor <natics07@gmail.com>
2022-11-22 21:04:24 -0500

    inclusión url railway

--- src/AxiosConect.js
+++ src/AxiosConect.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 //const BASE_URL = 'https://animalariumshop.herokuapp.com/'; conexion en despliegue
-const BASE_URL = 'http://localhost:3000';
+//const BASE_URL = 'http://localhost:3000';
+
+const BASE_URL = 'https://animalarium-back-production.up.railway.app/';
 
 const apiInstance = axios.create({baseURL:BASE_URL});
 
